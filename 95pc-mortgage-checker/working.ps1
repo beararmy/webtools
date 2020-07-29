4126a3d961b97164fb333fa64cc4e656f092954b
Stef Harrington-Palmer <stef@bear.army>
2020-07-29 22:06:24 +0100

    added more sites and comments on java/dom

--- 95pc-mortgage-checker/working.ps1
+++ 95pc-mortgage-checker/working.ps1
@@ -13,8 +13,19 @@ $negative_sites_to_check = @{
 
 # The site URL, then the string that implies yes
 $positive_sites_to_check = @{
-    "https://www.newcastle.co.uk/mortgages/our-mortgage-products/" = "class=`"table-rates__rate`">95`%</td>"
+    "https://www.newcastle.co.uk/mortgages/our-mortgage-products/"           = "class=`"table-rates__rate`">95%</td>"
+    "https://mortgages.firstdirect.com/mortgage-rates-fees/list-rates"       = "with 95% LTV and interest rate"
+    "https://www.leedsbuildingsociety.co.uk/mortgages/fixed-rate-mortgages/" = "`"Max LTV`">95%"
+    "https://www.skipton.co.uk/mortgages/first-time-buyers/products"         = "95%"
+    "https://www.ibs.co.uk/mortgages/our-mortgage-products/fixed-rate"       = "95%"
+    "https://www.postoffice.co.uk/mortgages/fixed-rate"                      = "(95% loan to value)"
 }
+
+
+## NEEDS WORK
+# https://www.coventrybuildingsociety.co.uk/content/cbs/consumer/en/mortgages/buying-a-home/mortgage-rates.html, java/dom doesn't render ltv in PS., oooh, maybe .Parsed HTML to get the same DOM as a browser?
+# nationwide, requires submitted queries
+
 
 $negative_sites_to_check.GetEnumerator() | ForEach-Object {
     $site = Invoke-WebRequest -Uri $_.key -UserAgent $userAgent -Method Get
